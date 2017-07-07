dec4a759fed57df3e6000eea84de762ed2d066cb
cfernandes <cfernandes@loyalty.com>
2017-07-07 11:20:18 -0400

    Implement certificate backup functionality

--- bin/operations/backup.js
+++ bin/operations/backup.js
@@ -1,5 +1,6 @@
 'use strict';
 const fs = require('fs');
+const {dissoc, merge, compose} = require('ramda');
 const colors = require('colors');
 const kongContext = require('../../kong/context');
 const kongApi = require('../../kong/index');
@@ -16,9 +17,35 @@ module.exports = async function backup(filename, url, username, password) {
     const apis = await kong.apis.allApis();
     const plugins = await kong.plugins.allPlugins();
     const consumers = await kong.consumers.allEnrichedConsumers();
-    const results = {apis, plugins, consumers};
+    const certificates = await kong.certificates.allCertificates();
+    const certificatesWithoutSNIs = certificates.map(certificate => dissoc('snis', certificate));
 
-    // TODO: Figure out how to do the backup process for certificates (should we dump the public key and private key in the same folder based on id?)
+
+    // create certificates directory if it doesn't already exist
+    if (!fs.existsSync('certificates')) {
+      fs.mkdirSync('certificates');
+    }
+
+    // write certificate key-pairs to file and replace the data with the file-paths on the JSON object
+    const modifiedCertificates = certificatesWithoutSNIs.map(certificate => {
+      const publicKey = certificate.cert;
+      const privateKey = certificate.key;
+      const id = certificate.id;
+      const publicKeyFilePath = `certificates/${id}.pub.pem`;
+      const privateKeyFilePath = `certificates/${id}.pem`;
+      fs.writeFileSync(publicKeyFilePath, publicKey);
+      fs.writeFileSync(privateKeyFilePath, privateKey);
+      const certificateMinusCertAndKey = compose(dissoc('cert'), dissoc('key'))(certificate);
+      return merge(certificateMinusCertAndKey, { cert_path: publicKeyFilePath, key_path: privateKeyFilePath });
+    });
+
+    const results = {
+      apis,
+      plugins,
+      consumers,
+      certificates: modifiedCertificates
+    };
+
     // TODO: implement the backup process for SNIs
 
     fs.writeFileSync(adjustedFileName, JSON.stringify(results, null, 4));
