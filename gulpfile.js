6c83500c4e86d05358b90b56d9eef648b90ba397
cfernandes <cfernandes@loyalty.com>
2017-07-08 03:28:31 -0400

    Since async-await and code coverage reporting don't play well, remove code coverage reporting Remove extra libraries

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,24 @@
 
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
-const istanbul = require('gulp-istanbul');
 const gutil = require('gulp-util');
 const eslint = require('gulp-eslint');
 
-const sourceFiles = ['index.js', 'lib/**/*.js', 'bin/**/*.js'];
+const sourceFiles = ['lib/**/*.js', 'bin/**/*.js'];
 const testSourceFiles = ['test/**/**.spec.js'];
 const allSourceFiles = sourceFiles.concat(testSourceFiles);
 
-gulp.task('test', done => {
-  gulp.src(sourceFiles)
-    // .pipe(istanbul())                     // Covering files
-    // .pipe(istanbul.hookRequire())         // Force `require` to return covered files
-    .on('finish', function () {
-      return gulp.src(testSourceFiles)
-        .pipe(mocha())
-        .on('error', gutil.log)
-        // .pipe(istanbul.writeReports())    // Creating the reports after tests ran
-        // .pipe(istanbul.enforceThresholds({thresholds: {global: 100}})) // Enforce a coverage of at least 100%
-        .on('end', done);
-    })
-    .on('error', gutil.log);
-});
+gulp.task('test', () =>
+  gulp.src(testSourceFiles)
+    .pipe(mocha())
+    .on('error', gutil.log)
+);
 
-gulp.task('lint', _ => {
-  return gulp.src(allSourceFiles)
+gulp.task('lint', () =>
+  gulp.src(allSourceFiles)
     .pipe(eslint({fix: true}))
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+    .pipe(eslint.failAfterError())
+);
 
 gulp.task('default', ['lint', 'test']);
