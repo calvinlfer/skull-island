72ae9e78a488efb4b8a6a1377fcf004f5fc1eb28
cfernandes <cfernandes@loyalty.com>
2017-07-10 20:06:26 -0400

    Flesh out other tests cases for consumer spec

--- test/consumers.spec.js
+++ test/consumers.spec.js
@@ -6,6 +6,7 @@ const kongContext = require('../lib/kong/context');
 const kongApi = require('../lib/kong/index');
 const localKongHost = 'http://127.0.0.1:8001';
 const delay = millis => new Promise(resolve => setTimeout(() => resolve(), millis));
+const DelayTime = 100;
 
 describe('Kong Consumer Object Specification', () => {
   it('must be able to create a consumer and list all consumers', async () => {
@@ -41,7 +42,7 @@ describe('Kong Consumer Object Specification', () => {
     });
 
     // Kong doesn't process this instantly so we wait
-    await delay(500);
+    await delay(DelayTime);
 
     const result = await kong.consumers.consumerDetails(consumerId, 'key-auth');
     const keyAuth = result[0];
@@ -50,8 +51,92 @@ describe('Kong Consumer Object Specification', () => {
     expect(keyAuth.key).to.be.equal(apiKey);
   });
 
-  it('must be able to remove a consumer and its credentials (without touching basic authentication)', () => {
+  it('must be able to remove a consumer and its credentials (without touching basic authentication)', async () => {
+    const context = kongContext('', '', localKongHost);
+    const kong = kongApi(context);
+    const consumerUsername = 'calvin-consumer-3';
+    const consumerId = "55017133-d70c-4659-bb91-ad23d5c81165";
+    const apiKey = '8544031257b140debd2b9aef089283f5';
+    const synchronizeBasicAuth = true;
+    await kong.consumers.createOrUpdateConsumerWithCredentials({
+      credentials: {
+        "key-auth": [
+          {
+            created_at: 1499722969000,
+            consumer_id: consumerId,
+            key: apiKey
+          }
+        ],
+        "basic-auth": [
+          {
+            password: "a-random-password",
+            consumer_id: consumerId,
+            username: "cal",
+            created_at: 1499286562000
+          }
+        ]
+      },
+      username: consumerUsername,
+      id: consumerId,
+      created_at: 1499722718000
+    }, synchronizeBasicAuth);
 
+    // Kong doesn't process this instantly so we wait
+    await delay(DelayTime);
+    await kong.consumers.cleanConsumerWithCredentials(consumerId);
+
+    // Kong doesn't process this instantly so we wait
+    await delay(DelayTime);
+
+    const result = await kong.consumers.allEnrichedConsumers();
+    expect(result).to.be.lengthOf(1);
+    const firstResult = result[0];
+    expect(firstResult.credentials['key-auth']).to.be.lengthOf(0);
+    expect(firstResult.credentials['basic-auth']).to.be.lengthOf(1);
+    const basicAuth = firstResult.credentials['basic-auth'][0];
+    expect(basicAuth.username).to.be.equal('cal');
+    expect(basicAuth.consumer_id).to.be.equal(consumerId);
+  });
+
+  it('must be able to completely remove a consumer', async () => {
+    const context = kongContext('', '', localKongHost);
+    const kong = kongApi(context);
+    const consumerUsername = 'calvin-consumer-4';
+    const consumerId = "65017133-d70c-4659-bb91-ad23d5c81167";
+    const apiKey = '8544031257b140debd2b9aef089283f5';
+    const synchronizeBasicAuth = true;
+    await kong.consumers.createOrUpdateConsumerWithCredentials({
+      credentials: {
+        "key-auth": [
+          {
+            created_at: 1499722969000,
+            consumer_id: consumerId,
+            key: apiKey
+          }
+        ],
+        "basic-auth": [
+          {
+            password: "a-random-password",
+            consumer_id: consumerId,
+            username: "cal",
+            created_at: 1499286562000
+          }
+        ]
+      },
+      username: consumerUsername,
+      id: consumerId,
+      created_at: 1499722718000
+    }, synchronizeBasicAuth);
+
+    // Kong doesn't process this instantly so we wait
+    await delay(DelayTime);
+    await kong.consumers.removeConsumerWithCredentials(consumerId);
+
+    // Kong doesn't process this instantly so we wait
+    await delay(DelayTime);
+
+    const result = await kong.consumers.allEnrichedConsumers();
+    expect(result).to.be.lengthOf(0);
   });
 
   afterEach(async () => {
